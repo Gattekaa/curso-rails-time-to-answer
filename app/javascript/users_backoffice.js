8d0a80020037476de3ce87ee6707ef37a1bff141
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-12-09 11:43:34 -0300

    Added user update page

--- app/javascript/users_backoffice.js
+++ app/javascript/users_backoffice.js
@@ -2,7 +2,5 @@
 //= require bootstrap/dist/js/bootstrap
 //= require fastclick/lib/fastclick
 //= require nprogress/nprogress
-//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js
-//= require jquery-ui/ui/widgets/datepicker
-//= require jquery-ui/ui/i18n/datepicker-pt-BR
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
 //= require custom
