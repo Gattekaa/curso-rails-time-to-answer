998952fd4c8fb2ec9b9f4f2e96e48428861223a7
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-12-13 09:19:32 -0300

    Added password recovery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,16 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '06ee17ee479f90',
+    :password => '9cf46f1e300be0',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
